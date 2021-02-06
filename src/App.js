0e454767db7fe0f5e3cc3bb810d1871fe4381dbf
nasimqureshi <qureshi.nasim@yahoo.com>
2021-02-05 21:18:24 -0600

    foth commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Status extends Component{
+  constructor(){
+    super();
+    this.state={
+      likes: 0
+    }
+    this.like = this.like.bind(this);
+  }
+  like(){
+    this.setState({
+      likes: this.state.likes + 1
+    })
+  }
+  
   render(){
     return (
       <div className="status">
         <p>{this.props.mytext}</p>
-        <p><button>Like</button></p>
+        <p>
+        <button onClick={this.like}>
+        {this.state.likes} Likes
+        </button></p>
       </div>
     )
   }
